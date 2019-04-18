c46043878400304f12c1f9a7b472d8c05f0afb01
Gabriel Konat <gabrielkonat@gmail.com>
2019-04-18 10:52:47 +0200

    Rename depconstraints project to prevent conflicts.

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-  api(platform(project(":depconstraints")))
+  api(platform(project(":log.depconstraints")))
 
   compileOnly("org.checkerframework:checker-qual-android")
 }
