f29981d8fb4cf31396f546e1fcbf01b77c3a8e2e
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-07-09 16:09:38 +0200

    Fix 'Expression expected'

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,6 @@ plugins {
     alias(libs.plugins.metaborg.convention.rootproject)
     alias(libs.plugins.gitonium)
 }
+
+// Workaround for issue: https://github.com/gradle/gradle/issues/20131
+println("")
