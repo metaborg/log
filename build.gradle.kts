28cfa3b976967d201b729ee1c7c4db1eb6c5b0cd
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-07-17 02:39:01 +0200

    Use dependencyResolutionManagement for repos

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,9 +18,4 @@ allprojects {
             repoName.set("log")
         }
     }
-
-    repositories {
-        maven("https://artifacts.metaborg.org/content/groups/public/")
-        mavenCentral()
-    }
 }
